37b75fb73239057ef3ba82118475dbe3eb080c83
Kiss Levente <kiss.levente@stud.uni-obuda.hu>
2021-11-07 12:05:56 +0100

    Settlement CRUD methods done

--- EFCUTY_HFT_2021221.Repository/SettlementRepository.cs
+++ EFCUTY_HFT_2021221.Repository/SettlementRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EFCUTY_HFT_2021221.Data;
+using EFCUTY_HFT_2021221.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,36 @@ using System.Threading.Tasks;
 
 namespace EFCUTY_HFT_2021221.Repository
 {
-    class SettlementRepository
+    public class SettlementRepository : ISettlementRepository
     {
+        WorldDbContext db;
+        public SettlementRepository(WorldDbContext db)
+        {
+            this.db = db;
+        }
+
+        public void Create(Settlement settlement)
+        {
+            db.Settlements.Add(settlement);
+            db.SaveChanges();
+        }
+
+        public Settlement Read(int id)
+        {
+            return db.Settlements.FirstOrDefault(t => t.SettlementID == id);
+        }
+
+        public void Update(Settlement settlement)
+        {
+            var oldSettlement = Read(settlement.SettlementID);
+            oldSettlement.Population = settlement.Population;
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            db.Remove(Read(id));
+            db.SaveChanges();
+        }
     }
 }
