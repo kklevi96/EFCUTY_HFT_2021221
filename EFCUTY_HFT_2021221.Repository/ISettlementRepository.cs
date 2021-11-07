b18939820f776dc33fff34bb662c52c56efa748c
Kiss Levente <kiss.levente@stud.uni-obuda.hu>
2021-11-07 12:06:09 +0100

    Settlement Repository Inteface generated

---
+++ EFCUTY_HFT_2021221.Repository/ISettlementRepository.cs
@@ -0,0 +1,12 @@
+﻿using EFCUTY_HFT_2021221.Models;
+
+namespace EFCUTY_HFT_2021221.Repository
+{
+    public interface ISettlementRepository
+    {
+        void Create(Settlement settlement);
+        void Delete(int id);
+        Settlement Read(int id);
+        void Update(Settlement settlement);
+    }
+}
