28fed1951f308a9e94e120812431f6aa108116ee
Kiss Levente <kiss.levente@stud.uni-obuda.hu>
2021-11-07 11:49:15 +0100

    Hello World removed

--- EFCUTY_HFT_2021221.Client/Program.cs
+++ EFCUTY_HFT_2021221.Client/Program.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using System.Linq;
+using System;
 
 namespace EFCUTY_HFT_2021221.Client
 {
+    //todo project reference data DELETE !! now it is added for testing only
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
