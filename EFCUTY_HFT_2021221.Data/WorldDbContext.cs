bcf767d18af94375d0c2c74271c303a17a87d765
Kiss Levente <kiss.levente@stud.uni-obuda.hu>
2021-11-26 16:00:04 +0100

    New data sample

--- EFCUTY_HFT_2021221.Data/WorldDbContext.cs
+++ EFCUTY_HFT_2021221.Data/WorldDbContext.cs
@@ -52,30 +52,167 @@ namespace EFCUTY_HFT_2021221.Data
                     .OnDelete(DeleteBehavior.Restrict);
 
                 entity
-                    .HasOne(citizen => citizen.HomeSettlement)
+                    .HasOne(citizen => citizen.Settlement)
                     .WithMany(settlement => settlement.Citizens)
                     .HasForeignKey(citizen => citizen.SettlementID)
                     .OnDelete(DeleteBehavior.Restrict);
 
             });
 
-            Country Canada = new() { CountryID = 1 };
-            Country Hungary = new() { CountryID = 2 };
-            Country Ukraine = new() { CountryID = 3 };
-            Country Bulgaria = new() { CountryID = 4 };
+            Country Hungary = new() //9.75M
+            {
+                CountryID = 1,
+                Name = "Hungary",
+                IsOECDMember = true,
+                TotalGDPInMillionUSD = 155000
+            };
 
-            Settlement Plovdiv = new() { SettlementID = 1, CountryID = 4, Population = 180000 };
-            Settlement Budapest = new() { SettlementID = 2, CountryID = 2, Population = 1800000 };
-            Settlement Kiev = new() { SettlementID = 3, CountryID = 3, Population = 3500000 };
-            Settlement Lvov = new() { SettlementID = 4, CountryID = 4, Population = 1850000 };
+            //Country Slovakia = new() //5.459M
+            //{
+            //    CountryID = 2,
+            //    Name = "Slovakia",
+            //    IsOECDMember = true,
+            //    TotalGDPInMillionUSD = 104600
+            //};
+            //Country Cameroon = new() //26.55M
+            //{
+            //    CountryID = 3,
+            //    Name = "Cameroon",
+            //    IsOECDMember = false,
+            //    TotalGDPInMillionUSD = 39800
+            //};
+            //Country NewZealand = new() //5.084M
+            //{
+            //    CountryID = 4,
+            //    Name = "New Zealand",
+            //    IsOECDMember = true,
+            //    TotalGDPInMillionUSD = 212500
+            //};
+            //Country Nauru = new() //10384
+            //{
+            //    CountryID = 5,
+            //    Name = "Nauru",
+            //    IsOECDMember = false,
+            //    TotalGDPInMillionUSD = 118
+            //};
+            //Country Spain = new() //47,35M
+            //{
+            //    CountryID = 6,
+            //    Name = "Spain",
+            //    IsOECDMember = true,
+            //    TotalGDPInMillionUSD = 1281000
+            //};
+            //Country USA = new() //329,5M
+            //{
+            //    CountryID = 7,
+            //    Name = "United States of America",
+            //    IsOECDMember = true,
+            //    TotalGDPInMillionUSD = 20940000
+            //};
+            //Country Bulgaria = new() //6.927M
+            //{
+            //    CountryID = 8,
+            //    Name = "Bulgaria",
+            //    IsOECDMember = false,
+            //    TotalGDPInMillionUSD = 69110
+            //};
+            //Country Slovenia = new() //2100000
+            //{
+            //    CountryID = 9,
+            //    Name = "Slovenia",
+            //    IsOECDMember = true,
+            //    TotalGDPInMillionUSD = 52880
+            //};
+            //Country Russia = new() //144100000
+            //{
+            //    CountryID = 10,
+            //    Name = "Russia",
+            //    IsOECDMember = false,
+            //    TotalGDPInMillionUSD = 1483000
+            //};
+            
 
-            Citizen citizen = new() { PersonID = 1, BirthDate = new DateTime(1996, 12, 11), Name = "Török Tamás", SettlementID = 2, CitizenshipID = 2 };
-            Citizen citizen2 = new() { PersonID = 2, BirthDate = new DateTime(1647, 5, 13), Name = "Orbán Viktor", SettlementID = 2, CitizenshipID = 2 };
-            Citizen citizen3 = new() { PersonID = 3, BirthDate = new DateTime(1920, 12, 11), Name = "Joe Trump", SettlementID = 4, CitizenshipID = 1 };
+            Settlement Budapest = new()
+            {
+                SettlementID = 1,
+                SettlementName = "Budapest",
+                CountryID = Hungary.CountryID,
+                HDI = 0.925,
+                Population = 1756000
+            };
 
-            builder.Entity<Country>().HasData(Canada, Hungary, Ukraine, Bulgaria);
-            builder.Entity<Settlement>().HasData(Plovdiv, Budapest, Kiev, Lvov);
-            builder.Entity<Citizen>().HasData(citizen, citizen2, citizen3);
+            Settlement Debrecen = new()
+            {
+                SettlementID = 2,
+                SettlementName = "Debrecen",
+                CountryID = Hungary.CountryID,
+                HDI = 0.893,
+                Population = 202520
+            };
+
+            Settlement Miskolc = new()
+            {
+                SettlementID = 3,
+                SettlementName = "Miskolc",
+                CountryID = Hungary.CountryID,
+                HDI = 0.833,
+                Population = 157639
+            };
+
+            Settlement OtherHungarianSettlements = new()
+            {
+                SettlementID = 4,
+                SettlementName = "Other Hungarian Settlements",
+                CountryID = Hungary.CountryID,
+                HDI = 0.899,
+                Population = 7567000
+            };
+
+            //Settlement Bratislava = new()
+            //{
+            //    SettlementID = 5,
+            //    SettlementName = "Bratislava",
+            //    CountryID = Slovakia.CountryID,
+            //    HDI = 0.945,
+            //    Population = 424428
+            //};
+
+            //Settlement Kosice = new()
+            //{
+            //    SettlementID = 6,
+            //    SettlementName = "Košice",
+            //    CountryID = Slovakia.CountryID,
+            //    HDI = 0.873,
+            //    Population = 239171
+            //};
+
+            //Settlement OtherSlovakSettlements = new()
+            //{
+            //    SettlementID = 7,
+            //    SettlementName = "Other Slovak Settlements",
+            //    CountryID = Slovakia.CountryID,
+            //    HDI = 0.912,
+            //    Population = 4800000
+            //};
+
+            Citizen myself = new()
+            {
+                PersonID = 1,
+                BirthDate = new DateTime(1996, 05, 22),
+                CitizenshipID = Hungary.CountryID,
+                SettlementID = Budapest.SettlementID,
+                HasCriminalRecord = false,
+                IncomeInUSD = 2000,
+                Name = "Levente Kiss"
+            };
+
+            //builder.Entity<Country>().HasData(Hungary, Slovakia, Cameroon, NewZealand, Nauru);
+            builder.Entity<Country>().HasData(Hungary);
+
+            //builder.Entity<Settlement>().HasData(Budapest, Debrecen, Miskolc, OtherHungarianSettlements, Bratislava, Kosice, OtherSlovakSettlements);
+            builder.Entity<Settlement>().HasData(Budapest, Debrecen, Miskolc, OtherHungarianSettlements);
+
+            builder.Entity<Citizen>().HasData(myself);
 
         }
     }
