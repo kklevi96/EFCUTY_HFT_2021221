0a88f0144aae5c6037c5373b10f88419013ac410
Kiss Levente <kiss.levente@stud.uni-obuda.hu>
2022-03-13 12:26:18 +0100

    main basics of ui - countries are now listed

---
+++ EFCUTY_HFT_2021221.WPFClient/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using EFCUTY_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCUTY_HFT_2021221.WPFClient
+{
+    public class MainWindowViewModel
+    {
+        public RestCollection<Country> Countries { get; set; }
+
+        //public ICommand Create
+
+        public MainWindowViewModel()
+        {
+            Countries = new RestCollection<Country>("http://localhost:54726/", "country");
+        }
+    }
+}
