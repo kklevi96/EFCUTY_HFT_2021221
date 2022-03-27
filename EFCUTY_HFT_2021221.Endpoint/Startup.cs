8ac7a247c0f3a32199eeb016e1410b62f1026bbb
Kiss Levente <kiss.levente@stud.uni-obuda.hu>
2022-03-27 19:35:51 +0200

    Swagger installed

--- EFCUTY_HFT_2021221.Endpoint/Startup.cs
+++ EFCUTY_HFT_2021221.Endpoint/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace EFCUTY_HFT_2021221.Endpoint
 {
@@ -22,6 +23,10 @@ namespace EFCUTY_HFT_2021221.Endpoint
             services.AddTransient<ISettlementRepository, SettlementRepository>();
             services.AddTransient<ICitizenRepository, CitizenRepository>();
             services.AddTransient<WorldDbContext, WorldDbContext>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "EFCUTY_HFT_2021221.Endpoint", Version = "v1" });
+            });
             services.AddSignalR();
             //services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
 
@@ -34,6 +39,8 @@ namespace EFCUTY_HFT_2021221.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EFCUTY_HFT_2021221.Endpoint v1"));
             }
 
             app.UseRouting();
